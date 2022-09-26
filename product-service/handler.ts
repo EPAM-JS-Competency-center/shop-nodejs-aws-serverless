9fdc459d3987ca3f3ccaa7caa77c6facc3b467d9
NhanTran <thanhnhan33.95@gmail.com>
2022-09-26 13:46:24 +0700

    use InMemoryService

--- product-service/handler.ts
+++ product-service/handler.ts
@@ -1,12 +1,7 @@
 import * as handlers from './src';
-import { Client } from 'pg';
-import { PostgresProductService } from './src/services/postgres-memory-product-service';
+import { InMemoryProductService } from './src/services/in-memory-product-service';
 
-console.log(process.env);
-
-const databaseClient = new Client();
-databaseClient.connect();
-const productService = new PostgresProductService(databaseClient)
+const productService = new InMemoryProductService();
 
 export const getProductById = handlers.getProductByIdHandler(productService);
 export const getAllProducts = handlers.getAllProductsHandler(productService);
